36affee867ea8421130d4f0192e2670f3c7d8030
nihalhussain14 <166994516+nihalhussain14@users.noreply.github.com>
2025-01-25 21:58:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("#btn");
+let content = document.querySelector("#container");
+let box = document.querySelector(".box");
+btn.addEventListener("click", (e) => {
+  box.style.zIndex = "1";
+  box.style. animation = "5s linear 0s show";
+});
